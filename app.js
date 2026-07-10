3b36bb2999317592de954a622b9851cfad9a21d5
GabrielMendesR <mendes@edu.unifil.br>
2026-07-10 18:53:26 -0300

    Bootstrap&PrimeirasViews

--- app.js
+++ app.js
@@ -2,27 +2,55 @@ import express from "express"
 import { engine } from "express-handlebars"
 import bodyParser from "body-parser"
 //import mongoose from "mongoose"
+import path from "path"
+import { fileURLToPath } from "url"
+import { dirname } from "path"
+import admin from "./routes/admin.js"
+
 
 const app = express()
 
 //Configurações
+
+//Path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename)
+
+
 //Body Parses
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+
 //HandleBars
-app.engine("handlebars", handlebars({ defaultLayout: "main" }))
-app.set("view engine", "handlebars");
+app.engine("handlebars", engine({
+    defaultLayout: "main",
+    runtimeOptions: {
+        allowProtoMethodsByDefault: true,
+        allowProtoPropertiesByDefault: true
+    }
+}))
+app.set("view engine", "handlebars")
+
 //Mongoose
 //Em breve
 
 
+//Public
+app.use(express.static(path.join(__dirname,"public")))
+
+
 //Rotas
+app.use('/admin', admin)
 
+app.get("/posts", (req, res) => {
+    res.send("Lista de Posts")
+})
 
+app.get("/", (req,res) => {
+    res.send("Pagina Inicial")
+})
 
 
 //Outros
 const PORT = 8081;
-app.listen(PORT, () => {
-    console.log("Servidor Rodando! ")
-})
+app.listen(PORT, () => console.log("Servidor Rodando!"))
---
+++ routes/admin.js
@@ -0,0 +1,19 @@
+import express from "express"
+
+const router = express.Router()
+
+
+router.get('/', (req,res) => {
+    res.render("admin/index")
+})
+
+router.get('/posts', (req, res) => {
+    res.send("Página de Posts")
+})
+
+router.get('/categorias', (req,res) => {
+    res.send("Pagina de Categorias")
+})
+
+
+export default router;
