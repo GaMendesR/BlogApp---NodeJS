8d8685693dbd0f35763fd97b7ff56348d2e178c7
GabrielMendesR <mendes@edu.unifil.br>
2026-07-17 00:32:48 -0300

    FuncionalidadeEditarCategorias

--- routes/admin.js
+++ routes/admin.js
@@ -62,4 +62,54 @@ router.post("/categorias/nova", (req, res) => {
     }
 })
 
+
+router.get("/categorias/edit/:id", (req, res) => {
+    Categoria.findOne({_id:req.params.id}).then((categoria) => {
+        res.render("admin/editcategorias", {categoria: categoria})
+    }).catch((err) => {
+        req.flash("error_msg", "Essa categoria não existe!")
+        res.redirect("/admin/categorias")
+    })
+})
+
+router.post("/categorias/edit", (req, res) => {
+    
+    var erros = []
+
+    if (!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null) {
+        erros.push({ texto: "Nome Inválido!" })
+    } else if (req.body.nome.length < 2) {
+        erros.push({ texto: "Nome Deve ser Maior que 2 caracteres!" })
+    }
+
+    if (!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null) {
+        erros.push({ texto: "Slug Inválido!" })
+    }
+
+
+    if (erros.length > 0) {
+        res.render("admin/editcategorias", {erros: erros})
+    } else {
+        Categoria.findOne({_id:req.body.id}).then((categoria) => {
+
+            categoria.nome = req.body.nome
+            categoria.slug = req.body.slug
+
+            categoria.save().then(() => {
+                req.flash("success_msg", "Categoria editada com sucesso!")
+                res.redirect("/admin/categorias")
+            }).catch((err) => {
+                req.flash("error_msg", "Houve um erro no DB ao editar a categoria, tente novamente!")
+                res.redirect("/admin/categorias")
+            })
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro ao editar a categoria, tente novamente!")
+            res.redirect("/admin/categorias")
+        })
+    }
+
+
+})
+
+
 export default router;
